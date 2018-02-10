54161bd0371a32778120a3fe282ec57b319650fb
hnguyen <hung135@hotmail.com>
2018-02-09 22:25:38 -0500

    addin binance

--- exchange/exchange.py
+++ exchange/exchange.py
@@ -1,64 +1,112 @@
 import pprint as pp
 
-class _coin:
-    price_now=0
-    price_yesterday=0
-    price_low_24hr=0
-    price_high_24hr=0
-    BITTREX_MAP={
-         'bid':'Bid',
-         'price_high_24hr':'High',
-         'price_now':'Last',
-         'price_low_24hr':'Low',
-         'market':'MarketName',
-         'total_buy':'OpenBuyOrders',
-         'total_sell':'OpenSellOrders',
-         'price_yesterday': 'PrevDay',
-         'Volume':'Volume'
-    }
 
-    def __init__(self,json,exchange='BITTREX'):
+class _coin:
+    bid = None
+    sell = None
+    last = None
+    price_yesterday = 0
+    price_low_24hr = 0
+    price_high_24hr = 0
+    BITTREX_MAP = {
+        'bid': 'Bid',
+        'sell': 'Sell',
+        'price_high_24hr': 'High',
+        'last': 'Last',
+        'price_low_24hr': 'Low',
+        'market': 'MarketName',
+        'total_buy': 'OpenBuyOrders',
+        'total_sell': 'OpenSellOrders',
+        'price_yesterday': 'PrevDay',
+        'volume': 'Volume'
+    }
+    BINANCE_MAP = {'market': 'symbol'
+        , 'bid': 'Bid'
+        , 'sell': 'Sell'
+        , 'bid': 'price'
+        , 'price_high_24hr': 'high'
+        , 'volume': 'volume'
+        , 'price_low_24hr': 'low'
+        , 'price_yesterday': 'prevClose'
+                   }
+
+    def __init__(self, json, exchange_map):
         if type(json) is not dict:
             raise Exception("json must be type dict")
-        for key,val in self.BITTREX_MAP.items():
-            #print(key)
-            #if key=='price_now':
-                #print(type(json))
-            setattr(self, key, json[val])
-                #print("test",self.price_now)
+        if type(exchange_map) is not dict:
+            raise Exception("exchange_map must be type dict")
+        # looks through json and pulls fields that matches the mapping and set the instance variable
+        for key, val in exchange_map.items():
+            value = json.get(val, 0)
+            if value != 0:
+                setattr(self, key, value)
+
+    def print_price(self):
+        print(self.bid,
+              self.sell,
+              self.price_yesterday,
+              self.price_low_24hr,
+              self.price_high_24hr)
 
 
 class Exchange:
-    msg="{}->{} Price {} Vol {}"
+    msg = "{}->{} Price {} Vol {}"
+    coin = None
 
-    def get_coin(self,coin):
+    def get_coin(self, coin):
         pass
-    def compare_price(self,hours,market):
 
-        x=self.my_bittrex.get_market_summary(market=market)
-        #price_now=x['Bid']
-        #price_yesterday=[]
-        #low=x['Low']
-        json=x['result']
-        print(type(json))
-        x=_coin(json=json[0])
-        #pp.pprint(x['result'])
-        print(x.price_now,x.price_yesterday,x.price_low_24hr)
+    def compare_price(self, hours, market):
+        pass
+        # x=self.my_bittrex.get_market_summary(market=market)
+        # price_now=x['Bid']
+        # price_yesterday=[]
+        # low=x['Low']
+        # json=x['result']
+        # print(type(json))
+        # x=_coin(json=json[0])
+        # pp.pprint(x['result'])
+        # print(x.price_now,x.price_yesterday,x.price_low_24hr)
+
+    def _do_bittrex(self, api_key, secrete_key, market):
+        from bittrex.bittrex import Bittrex, API_V2_0
+
+        # my_bittrex = Bittrex(None, None, api_version=API_V2_0)  # or defaulting to v1.1 as Bittrex(None, None)
+        self.my_bittrex = Bittrex(api_key, secrete_key)  # or defaulting to v1.1 as Bittrex(None, None)
+        self.my_bittrex.get_markets()
+        # print(my_bittrex,dir(my_bittrex))
+        self.summary = self.my_bittrex.get_market_summaries()
+        # pp.pprint(summary)
+
+        for coin in self.summary['result']:
+            if coin['MarketName'].find(market) > 0:
+                self.msg = self.msg.format(self.exchange, coin['MarketName'], coin['Bid'], coin['Volume'])
+
+    def _do_binance(self, api_key, secrete_key, market):
+        from binance.client import Client
+        client = Client(api_key, secrete_key)
+
+        # get market depth
+        depth = client.get_order_book(symbol=market).get('bids', "")
+        pp.pprint(depth[0])
+        # place market buy order
+        """order = client.create_order(
+            symbol=market,
+            side=Client.SIDE_BUY,
+            type=Client.ORDER_TYPE_MARKET,
+            quantity=100)"""
+        products = client.get_products()
+        # get all symbol prices
+        # prices = client.get_all_tickers()
+        # print(prices)
+        for coin in products['data']:
+            if coin['symbol'] == market:
+                # pp.pprint(coin)
+                self.coin = _coin(json=coin, exchange_map=_coin.BINANCE_MAP)
+
     def __init__(self, api_key=None, secrete_key=None, exchange='BITTREX', market='BTC-USDT'):
-        if exchange=='BITTREX':
-            from bittrex.bittrex import Bittrex, API_V2_0
-            # my_bittrex = Bittrex(None, None, api_version=API_V2_0)  # or defaulting to v1.1 as Bittrex(None, None)
-            self.my_bittrex = Bittrex(api_key, secrete_key)  # or defaulting to v1.1 as Bittrex(None, None)
-            self.my_bittrex.get_markets()
-            # print(my_bittrex,dir(my_bittrex))
-            self.summary = self.my_bittrex.get_market_summaries()
-            # pp.pprint(summary)
-
-            for coin in self.summary['result']:
-
-                if coin['MarketName'].find(market)> 0:
-
-                    self.msg=self.msg.format(exchange,coin['MarketName'],coin['Bid'],coin['Volume'])
-
-
-
+        self.exchange = exchange
+        if exchange == 'BITTREX':
+            self._do_bittrex(api_key, secrete_key, market)
+        if exchange == 'BINANCE':
+            self._do_binance(api_key, secrete_key, market)
--- send_sms.py
+++ send_sms.py
@@ -1,24 +1,31 @@
-
-import smtplib
-import os
+import smtplib, os, time
 from exchange import Exchange
 
 # Establish a secure session with gmail's outgoing SMTP server using your gmail account
-server = smtplib.SMTP( "smtp.gmail.com", 587 )
+server = smtplib.SMTP("smtp.gmail.com", 587)
 
 server.starttls()
-logon_id=os.getenv('logon_id')
-logon_pwd=os.getenv('logon_pwd')
-phone=os.getenv('phone')
+logon_id = os.getenv('logon_id')
+logon_pwd = os.getenv('logon_pwd')
+binance_api = os.getenv('api_key')
+binacne_secret = os.getenv('secret_key')
+
+phone = os.getenv('phone')
 print(logon_id)
 print(logon_pwd)
 server.login(logon_id, logon_pwd)
 
 # Send text message through SMS gateway of destination number
 
-ex=Exchange(exchange='BITTREX',market='BTC')
-ex.compare_price(hours=24,market='BTC-NEO')
-print(str(ex.msg))
+# ex=Exchange(exchange='BITTREX',market='BTC')
+# ex.compare_price(hours=24,market='BTC-BCC')
+ex = Exchange(exchange='BINANCE', market='TRXBTC')
+#
+print(ex.coin.print_price())
 
-#server.sendmail( logon_id, phone, str(z.msg))
-
+# While loop
+while (True):
+    print("looping...")
+    time.sleep(20)
+    # ex.compare_price(hours=24, market='BTC-NEO')
+# server.sendmail( logon_id, phone, str(z.msg))
