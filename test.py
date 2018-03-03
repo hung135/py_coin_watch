bd80734d48da1fb9c26236060403d82cc59c163d
Hung Nguyen <hung135@hotmail.com>
2018-03-02 20:23:24 -0500

    travis ci test

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print("hello")
-print("hello2")
+import unittest
+
+import crypto
+class TestCoin(unittest.TestCase):
+    def test_init(self):
+        print("hello")
+        self.assertEqual('s',1)
+if __name__ == '__main__':
+    unittest.main()
