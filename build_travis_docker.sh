2969a0926a3e0bbaac2691234d93487b2c9a45f4
Hung Nguyen <hung135@hotmail.com>
2018-03-03 23:47:32 -0500

    adding mount folder into docker machine to see travis files

--- build_travis_docker.sh
+++ build_travis_docker.sh
@@ -1,18 +1,45 @@
 #!/usr/bin/env bash
-docker-machine start default
+export MACHINE_NAME="default"
+export VOL_DIR="_docker_travis"
+export WORKDIR=$(basename "$VOL_DIR")
+export PROJECT_PATH=$(pwd)
+export U=2000
+export G=2000
+diskutil unmount $PROJECT_PATH/$WORKDIR
+
+
+echo $PROJECT_PATH/$WORKDIR
+rm $PROJECT_PATH/$WORKDIR
+mkdir $PROJECT_PATH/$WORKDIR
+
+docker-machine ssh default sudo rm foo -rf
+docker-machine ssh default mkdir foo
+docker-machine ssh default chmod 777 foo -R
+
+docker-machine mount default:/home/docker/foo $PROJECT_PATH/$WORKDIR
+
+#docker-machine ssh $MACHINE_NAME "sudo mkdir -p \"$VOL_DIR\""
+#vboxmanage sharedfolder add "$MACHINE_NAME" --name "$WORKDIR" --hostpath "$VOL_DIR" --transient
+#docker-machine ssh $MACHINE_NAME "sudo mount -t vboxsf -o uid=\"$U\",gid=\"$G\" \"$WORKDIR\" \"$VOL_DIR\""
+
+docker-machine start $MACHINE_NAME
 docker-machine env
 eval $(docker-machine env)
 
 docker rm travis-debug -f
 # this is for a python build find yours Here: https://docs.travis-ci.com/user/common-build-problems/
 # under Running a Container Based Docker Image Locally #
-docker run --name travis-debug -dit travisci/ci-garnet:packer-1512502276-986baf0 /sbin/init
+#docker run --name travis-debug -dit travisci/ci-garnet:packer-1512502276-986baf0 /sbin/init
+#mount local dir
+mkdir travis
+pwd
+docker run --name travis-debug -v /home/docker/foo/:/home/travis/builds/ -dit travisci/ci-garnet:packer-1512502276-986baf0 /sbin/init
 
 
 docker exec --user travis travis-debug /bin/bash -c "
 export BRANCH=master;
 export PROJECT_PATH=hung135/py_coin_watch;
-source ~/.bash_profile; env;
+source ~/.bash_profile;
 cd /home/travis/builds/;
 git clone https://github.com/travis-ci/travis-build;
 mkdir ~/.travis/;
